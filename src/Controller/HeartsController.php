f6c94e1fc62187ebbc4ea3f352ee64d7b30f5fb4
ludovic_gendre <ludovic.gendre@g-mail.fr>
2021-01-16 15:36:12 +0100

    feature/injected title and description

--- src/Controller/HeartsController.php
+++ src/Controller/HeartsController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Heart;
+use Twig\Environment;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HeartsController extends AbstractController
 {
+  private $em;
+
+  public function _construct(Environment $twig,EntityManagerInterface $em)
+  {
+    dd($twig);
+    
+    $this->$em = $em;
+  }
     /**
      * @Route("/hearts", name="hearts")
      */
     public function index(): Response
     {
-        return $this->render('hearts/index.html.twig');
+      $heart = new Heart;
+      $heart ->  setTitle('Title 2');
+      $heart ->  setDescription('Description 2');
+
+      $em = $this->getDoctrine()->getManager();
+
+      $this -> em -> persist($heart);
+      $this -> em -> flush();
+
+      dump($heart);
+      return $this->render('hearts/index.html.twig');
     }
 }
