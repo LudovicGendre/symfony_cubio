36767fa2c67728a645a470b84fc788dbea5fbb8b
ludovic_gendre <ludovic.gendre@g-mail.fr>
2021-01-16 15:52:27 +0100

    feature/added first step favoris

--- src/Controller/HeartsController.php
+++ src/Controller/HeartsController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Heart;
-use Twig\Environment;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,29 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HeartsController extends AbstractController
 {
-  private $em;
-
-  public function _construct(Environment $twig,EntityManagerInterface $em)
-  {
-    dd($twig);
-    
-    $this->$em = $em;
-  }
     /**
      * @Route("/hearts", name="hearts")
      */
-    public function index(): Response
-    {
-      $heart = new Heart;
-      $heart ->  setTitle('Title 2');
-      $heart ->  setDescription('Description 2');
 
-      $em = $this->getDoctrine()->getManager();
+  public function index(EntityManagerInterface $em): Response
+  {
+    $repo = $em->getRepository(Heart::class);
 
-      $this -> em -> persist($heart);
-      $this -> em -> flush();
-
-      dump($heart);
-      return $this->render('hearts/index.html.twig');
-    }
+    $hearts = $repo->findAll();
+    return $this->render('hearts/index.html.twig', ['hearts' => $hearts]);
+  }
 }
