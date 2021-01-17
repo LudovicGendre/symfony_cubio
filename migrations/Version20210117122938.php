71f3d477fffd30b5cc6ac7b0353c46b509f99801
ludovic_gendre <ludovic.gendre@g-mail.fr>
2021-01-17 14:16:24 +0100

    feature/added create favoris

--- migrations/Version20210117122938.php
+++ migrations/Version20210117122938.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210116135701 extends AbstractMigration
+final class Version20210117122938 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,7 +20,7 @@ final class Version20210116135701 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE heart (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description CLOB NOT NULL)');
+        $this->addSql('CREATE TABLE heart (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
--- src/Controller/HeartsController.php
+++ src/Controller/HeartsController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Heart;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\HeartRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,11 +16,30 @@ class HeartsController extends AbstractController
      * @Route("/hearts", name="hearts")
      */
 
-  public function index(EntityManagerInterface $em): Response
+  public function index(HeartRepository $repo): Response
   {
-    $repo = $em->getRepository(Heart::class);
-
-    $hearts = $repo->findAll();
-    return $this->render('hearts/index.html.twig', ['hearts' => $hearts]);
+    return $this->render('hearts/index.html.twig', ['hearts'=> $repo->findAll()]);
   }
+   /**
+     * @Route("/hearts/create", methods={"GET", "POST"})
+     */
+  public function create(Request $request, EntityManagerInterface $em)
+  {
+    if($request->isMethod('POST')) {
+      $data = $request->request->all();
+
+      if ($this->isCsrfTokenValid('hearts_create', $data['_token'])) {
+        $heart = new Heart;
+        $heart->setTitle($data['title']);
+        $heart->setDescription($data['description']);
+        $em->persist($heart);
+        $em->flush();
+      }
+
+      return $this->redirect('/hearts');
+    } 
+
+    return $this->render('hearts/create.html.twig');
+  }
+
 }
